6d554ebc76c3f8dd787335a9beb1544c1cbafa2d
haewonee <godnjs5870@naver.com>
2025-04-30 14:00:47 +0900

    집에서 가져와서 써~

--- shop/src/main/java/com/apple/shop/Item.java
+++ shop/src/main/java/com/apple/shop/Item.java
@@ -1,18 +1,18 @@
 package com.apple.shop;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.ToString;
 
+@ToString
 @Entity
 public class Item {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    Long id; //알아서 1씩 증가시켜준다.
+    private Long id; //알아서 1씩 증가시켜준다.
 
-    String title;
+    private String title;
 
-    Integer price;
+    private Integer price;
 }
+
--- shop/src/main/java/com/apple/shop/ItemController.java
+++ shop/src/main/java/com/apple/shop/ItemController.java
@@ -1,15 +1,24 @@
 package com.apple.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class ItemController {
 
+    private final ItemRepository itemRepository;
+
     @GetMapping("/list")
     public String list(Model model){
-        model.addAttribute("name","홍길동");
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items",result);
+        var a = new Item();
+        System.out.println(a.toString());
         return "list";
     }
 }
